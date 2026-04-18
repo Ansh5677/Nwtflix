043eaca90d8a01405c0053924b535a84cb234fcb
Ansh5677 <anshsingh52950@gmail.com>
2026-04-18 11:06:06 +0530

    Create Next.js

---
+++ Next.js
@@ -0,0 +1,4 @@
+npx create-next-app@latest .
+# Select: Yes (Src Dir), Yes (App Router), Yes (Tailwind), Yes (ESLint)
+npm install mongoose lucide-react framer-motion clsx tailwind-merge react-hot-toast bcryptjs jsonwebtoken
+
